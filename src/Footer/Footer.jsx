4bcfe5ae4fc5a957ae05a7d934c8dd659f5af20e
Álvaro C <castillaloaizaalvaro@gmail.com>
2024-06-27 20:52:55 +0200

    DEPLOY READY

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -7,7 +7,7 @@ export function Footer(darkMode) {
         <div className='footer'>
             <hr className={darkMode ? 'hr-footer-dark' : 'hr-footer'}/>
             <h4 className={darkMode ? 'h4-footer-dark' : 'h4-footer'}>Developed by Álvaro Castilla. All rights reserved. © </h4>
-            <img src={darkMode ? "https://github.com/alvarocastillx/Liberty/blob/e33b0434cb2836b89a99306ce0773151d3b97fc1/src/assets/DEV%20LOGO%20WHITE.png" : "/src/assets/DEV LOGO.png"} alt="ACLLogo" width={60} />
+            <img src={darkMode ? "https://raw.githubusercontent.com/alvarocastillx/Liberty/e33b0434cb2836b89a99306ce0773151d3b97fc1/src/assets/DEV%20LOGO%20WHITE.png" : "https://raw.githubusercontent.com/alvarocastillx/Liberty/e33b0434cb2836b89a99306ce0773151d3b97fc1/src/assets/DEV%20LOGO.png"} alt="ACLLogo" width={60} />
         </div>
     )
 }
